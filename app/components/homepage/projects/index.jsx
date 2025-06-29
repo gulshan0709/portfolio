b665c9465a730a47d58244df464e9ac6dd469d8b
Gulshan Kumar <gulshan.kumar@realcooderz.in>
2025-06-29 15:48:58 +0530

    code

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -5,7 +5,6 @@ import Link from 'next/link';
 import ProjectCard from './project-card';
 
 const Projects = () => {
-console.log('/Projects')
   return (
     <div id='projects' className="relative z-50  my-12 lg:my-24">
       <div className="sticky top-10">
@@ -35,14 +34,14 @@ console.log('/Projects')
       </div>
       {projectsData.length>4?
        <div className="flex justify-center  mt-5 lg:mt-12">
-        {/* <Link
+        <Link
           className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
           role="button"
           href="/allproject"
         >
           <span>View More</span>
           <FaArrowRight size={16} />
-        </Link> */}
+        </Link>
       </div>
       :null}
     </div>
--- app/page.js
+++ app/page.js
@@ -38,7 +38,7 @@ export default async function Home() {
       {/* <Experience /> */}
       <Skills />
       {/* <Projects /> */}
-      {/* <Education /> */}
+      <Education />
       <Blog blogs={blogs} />
       <ContactSection />
     </div>
