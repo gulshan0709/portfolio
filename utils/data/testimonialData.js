fcd3fa1eb90a67209a1624a01d2df2364d52b7a7
Gulshan Kumar <gulshan.kumar@realcooderz.in>
2025-06-29 20:07:25 +0530

    code

--- utils/data/testimonialData.js
+++ utils/data/testimonialData.js
@@ -3,7 +3,7 @@ export const testimonialData = [
     id: 1,
     name: "Somnath Mishra",
     description:"Working with Gulshan has been an incredible experience. His deep understanding of software development, attention to detail, and proactive communication set him apart. He consistently delivers high-quality solutions and is always ready to tackle challenging problems with creativity and skill. Any team would be lucky to have him.",
-    role: "Senior AI Engineer",
+    role: "Senior AI/ML Engineer",
     compnay: "Realcoderz",
     photo:"/somnath.png"
   },
@@ -12,7 +12,7 @@ export const testimonialData = [
     name: "Sourabh Sunil Singh",
     description:
       "Working with Gulshan kumar has been one of the best parts of my journey. Always helpful, patient, and incredibly supportive. they’ve been more than just a senior; they've been a true mentor. No matter the time or situation, they were always there to guide and motivate. I genuinely admire their dedication and the positive energy they bring. Forever grateful to have learned under their guidance.",
-    role: "AI Engineer",
+    role: "AI/ML Engineer",
     compnay: "goML, (Ex-Realcoderz)",
     photo:"/sourabh_sunil.png"
   },
@@ -21,7 +21,7 @@ export const testimonialData = [
     name: "Saurabh Singh",
     description:
       "I have worked alongside Gulshan and can confidently say he is a dedicated, technically proficient professional. As a Senior Software Developer, he brings expertise, strong problem-solving skills, and ownership to every project. His understanding of software architecture, clean code practices, and scalable design, combined with his collaborative nature, make him a valuable asset.",
-    role: "Lead AI Engineer",
+    role: "Lead AI/ML Engineer",
     compnay: "HCL Tech, (Ex-Realcoderz)",
     photo:"/saurabh.png"
   },
