2cac14e29d30cb829cf10bd030ed62167bf14ce9
Gulshan Kumar <gulshan.kumar@realcooderz.in>
2025-06-29 16:03:46 +0530

    code

--- app/page.js
+++ app/page.js
@@ -37,7 +37,7 @@ export default async function Home() {
       <Experience />
       <Skills />
       <Projects />
-      {/* <Education /> */}
+      <Education />
       <Blog blogs={blogs} />
       <ContactSection />
     </div>
