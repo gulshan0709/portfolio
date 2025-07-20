5ca0606bcb0b0f1209bd7cd53d5f49e9eaf4db20
Gulshan Kumar <gulshan.kumar@realcooderz.in>
2025-07-20 22:28:25 +0530

    code

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -36,7 +36,7 @@ function ContactForm() {
     try {
       setIsLoading(true);
       const res = await axios.post(
-        `http://localhost:3000/api/contact`,
+        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
         userInput
       );
 
